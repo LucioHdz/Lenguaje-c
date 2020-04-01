6754a2a26e0b417ca251978442cdbafeb428626e
LucioHdz <57549359+LucioHdz@users.noreply.github.com>
2020-04-01 15:37:06 -0600

    Agregar Apuntadores

---
+++ Programas/Apuntadores/Ejemplo2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *p,y;
+void func(){
+  int x = 40;
+  p = &x;
+  y = *p;
+  *p = 23;
+}
+
+int main(void){
+  func();
+  y = *p;
+  *p = 25;
+  printf("El valor de y es %d\nEl valor de *p es %d\nEl valor de p es %p\n ",y,*p,p );
+}
